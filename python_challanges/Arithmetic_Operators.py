f09f1e8f72441285ddf071f88f9e4dad49e09997
luckysaintmolotov <209353814+luckysaintmolotov@users.noreply.github.com>
2025-05-09 22:24:14 +0200

    Create python file containing the barebones and task outlay and functions deemed needed

---
+++ python_challanges/Arithmetic_Operators.py
@@ -0,0 +1,16 @@
+'''Task
+The provided code stub reads two integers from STDIN, and
+
+. Add code to print three lines where:
+
+    The first line contains the sum of the two numbers.
+    The second line contains the difference of the two numbers (first - second).
+    The third line contains the product of the two numbers.'''
+    
+    
+def sum(a,b):
+    pass
+def diff(a,b):
+    pass
+def prod(a,b):
+    pass
