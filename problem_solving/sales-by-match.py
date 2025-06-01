c6a7f8b9e65e0f701f02fea4807aab23378603cb
luckysaintmolotov <209353814+luckysaintmolotov@users.noreply.github.com>
2025-06-01 23:54:19 +0200

    create new folder for the correct challanges

---
+++ problem_solving/sales-by-match.py
@@ -0,0 +1,20 @@
+"""
+There is a large pile of socks that must be paired by color. 
+Given an array of integers representing the color of each sock, 
+determine how many pairs of socks with matching colors there are.
+"""
+
+
+def sockMerchant(n, ar):
+    counts = {}
+    for i in ar:
+        counts[i] = counts.get(i, 0) + 1
+        pairs = 0
+    for count in counts.values():
+        pairs += count // 2
+    print(pairs)
+    return pairs
+
+n = 7
+ar = [1,2,1,2,1,3,2]
+sockMerchant(ar)
---
+++ problem_solving/simple-array-sum.py
@@ -0,0 +1,9 @@
+#
+# Complete the 'simpleArraySum' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY ar as parameter.
+#
+
+def simpleArraySum(ar):
+    return sum(ar)
