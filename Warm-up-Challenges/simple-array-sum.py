26167b26ae0ffd60cccc5faae2044c14fb207977
luckysaintmolotov <209353814+luckysaintmolotov@users.noreply.github.com>
2025-06-01 23:53:41 +0200

    move files to new folder

--- Warm-up-Challenges/simple-array-sum.py
+++
@@ -1,9 +0,0 @@
-#
-# Complete the 'simpleArraySum' function below.
-#
-# The function is expected to return an INTEGER.
-# The function accepts INTEGER_ARRAY ar as parameter.
-#
-
-def simpleArraySum(ar):
-    return sum(ar)
