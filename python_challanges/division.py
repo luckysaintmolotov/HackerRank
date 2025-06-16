27ae00c74600aba1c87c2a4a9ca4bbac6cfb44f4
luckysaintmolotov <209353814+luckysaintmolotov@users.noreply.github.com>
2025-06-17 01:08:14 +0200

    Delete python_challanges directory

--- python_challanges/division.py
+++
@@ -1,6 +0,0 @@
-a = int(input())
-b = int(input())
-result1 = a//b
-result2= a/b
-print(result1)
-print(result2)
--- python_challanges/printFunction.py
+++
@@ -1,4 +0,0 @@
-n=5
-listofnumbers = list(range(1,n))
-stringofnumbers = ''.join(str(i) for i in listofnumbers)
-print(stringofnumbers)
